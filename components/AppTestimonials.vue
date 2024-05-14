c5dbb7f1fec651ec4b8f2fadbba43eb9f2a0e440
Boluwatife2904 <sanvicola2000@gmail.com>
2024-05-14 20:54:19 +0100

    chore: complete ui implementation of testimonials section 💯

--- components/AppTestimonials.vue
+++ components/AppTestimonials.vue
@@ -1,37 +1,28 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section class="py-[90px] bg-white">
-    <div
-      class="app-container flex flex-col gap-6 md:gap-10 justify-center items-center text-center"
-    >
-      <p
-        class="uppercase font-bold text-lg md:text-[24px] -tracking-[2%] text-[#3782CA]"
-      >
-        testimonials
-      </p>
-      <h5
-        class="text-[#1B2435] font-bold text-[32px] md:text-[38px] lg:text-[48px] leading-[40px] md:leading-[52px] lg:leading-[72px] -tracking-[4%]"
-      >
-        From Our Customers
-      </h5>
-      <p
-        class="text-[#313131] max-w-[780px] mx-auto text-center text-lg md:text-[24px] leadin-6 md:leading-9"
-      >
-        Feedback occurs when outputs a system are routed back as a inputs as
-        parts of a chain of cause-and-effect that forms a circuit or loop
-      </p>
-      <ul class="grid gap-5 w-full max-w-[1120px] mx-auto">
-        <TestimonialCard />
-        <TestimonialCard />
-        <TestimonialCard />
-      </ul>
-    </div>
-  </section>
+    <section class="py-[90px] bg-white relative">
+        <AppIcon name="testimonials-bg-left" class="absolute top-0 left-0 hidden md:block" />
+        <AppIcon name="testimonials-bg-left" class="absolute bottom-[68px] right-0 hidden md:block" />
+        <div class="app-container flex flex-col gap-6 md:gap-10 justify-center items-center text-center">
+            <p class="uppercase font-bold text-lg md:text-[24px] -tracking-[2%] text-[#3782CA]">testimonials</p>
+            <h5 class="text-[#1B2435] font-bold text-[32px] md:text-[38px] lg:text-[48px] leading-[40px] md:leading-[52px] lg:leading-[72px] -tracking-[4%]">
+                From Our Customers
+            </h5>
+            <p class="text-[#313131] max-w-[780px] mx-auto text-center text-lg md:text-[24px] leadin-6 md:leading-9">
+                Feedback occurs when outputs a system are routed back as a inputs as parts of a chain of cause-and-effect that forms a circuit or loop
+            </p>
+            <ul class="grid gap-5 w-full max-w-[1160px] mx-auto">
+                <TestimonialCard />
+                <TestimonialCard />
+                <TestimonialCard />
+            </ul>
+        </div>
+    </section>
 </template>
 
 <style scoped>
 ul {
-  grid-template-columns: repeat(auto-fit, minmax(356px, 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(356px, 1fr));
 }
 </style>
--- components/TestimonialCard.vue
+++ components/TestimonialCard.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts"></script>
 
 <template>
-  <li class="bg-[#E7F1FC] rounded-[24px] py-[56px] px-10 flex flex-col gap-6">
-    <div class="flex flex-col gap-[10px] items-center justify-center text-center">
-      <h6 class="text-[#1B2435] font-bold text-[24px]">Aaron Aslhey</h6>
-      <p class="text-xl font-medium text-[#5E84A1]">
-        UI Designer at Mamahe Studio
-      </p>
-    </div>
-    <p class="text-base -tracking-[4%] text-center">
-      “ RepMedia made my life easy. It helped me with search for my first ever
-      job. Thanks to the team for providing this relevant tool. “
-    </p>
-  </li>
+    <li class="group bg-[#E7F1FC] hover:bg-[#1B2435] rounded-[24px] py-[56px] px-10 flex flex-col gap-6 relative z-1 transition-colors duration-500">
+        <img src="/images/testimonial.png" alt="testimonial" class="block mx-auto h-[60px] w-[60px] rounded-full shrink-0" />
+        <div class="flex flex-col gap-[10px] items-center justify-center text-center">
+            <h6 class="text-[#1B2435] font-bold text-[24px] group-hover:text-white transition-colors duration-200">Aaron Aslhey</h6>
+            <p class="text-xl font-medium text-[#5E84A1] group-hover:text-[#C6C6C6] group-hover:font-[450] transition-colors duration-200">
+                UI Designer at Mamahe Studio
+            </p>
+        </div>
+        <p class="text-base font-medium -tracking-[4%] text-center group-hover:text-[#FFFFFF] transition-colors duration-200">
+            “ RepMedia made my life easy. It helped me with search for my first ever job. Thanks to the team for providing this relevant tool. “
+        </p>
+    </li>
 </template>
 
 <style scoped></style>
