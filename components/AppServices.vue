a2ea8e1b834bd2d83b7820aac6a3709695cbc257
Boluwatife2904 <sanvicola2000@gmail.com>
2024-05-14 23:32:18 +0100

    chore: implement ui for what we do section 👨🏽‍🔧

---
+++ components/AppServices.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+const services = [
+    {
+        image: "resume",
+        title: "Resume/CV review",
+        content: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form",
+    },
+    {
+        image: "cover-letter",
+        title: "Cover Letter",
+        content: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form",
+    },
+    {
+        image: "linkedin",
+        title: "LinkedIn Optimization",
+        content: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form",
+    },
+    {
+        image: "business",
+        title: "Business Plan",
+        content: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form",
+    },
+    {
+        image: "portfolio",
+        title: "Portfolio Review",
+        content: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form",
+    },
+    {
+        image: "biography",
+        title: "Create a Biography",
+        content: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form",
+    },
+];
+</script>
+
+<template>
+    <section class="py-[80px]">
+        <div class="app-container flex flex-col gap-5 items-center justify-center">
+            <p class="section-title !normal-case">What we do</p>
+            <div class="space-y-10 w-full">
+                <h4 class="section-header text-center">Personal or Corporate Branding?</h4>
+                <ul class="services grid gap-5 max-w-[1200px] mx-auto w-full">
+                    <ServicesCard v-for="service in services" :key="service.image" :service />
+                </ul>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+.services {
+    grid-template-columns: repeat(auto-fit, minmax(330px, 1fr));
+}
+</style>
---
+++ components/ServicesCard.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+interface Service {
+    image: string;
+    title: string;
+    content: string;
+}
+
+interface Props {
+    service: Service;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+    <li
+        class="group p-5 border border-[#CACACA] bg-[#EFEFEF] rounded-[24px] flex flex-col gap-y-5 hover:border-[#3882CB] hover:bg-[#E7F1FC] transition-colors duration-300">
+        <img :src="`/images/${service.image}.png`" :alt="`${service.title}`" class="h-20 w-20" />
+        <div class="flex flex-col gap-[15px]">
+            <h6 class="text-[#010101] text-[24px] leading-9 font-medium">{{ service.title }}</h6>
+            <p class="text-[#010101] opacity-60 text-base">{{ service.content }}</p>
+            <button
+                class="flex items-center gap-1 bg-white text-[#0F0F0F] border border-[#000] rounded-[24px] py-[10px] px-[24px] w-fit group-hover:bg-black group-hover:text-white transition-colors duration-500 ease-in-out">
+                Get Started
+                <AppIcon name="arrow-right-line" />
+            </button>
+        </div>
+    </li>
+</template>
+
+<style scoped></style>
